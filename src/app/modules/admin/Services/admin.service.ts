f2d518d1c39034290a09f846ba4271dca6eeb104
ram kumbhre <ramkumbhre321@gmail.com>
2025-10-29 17:03:44 +0530

    V.5

--- src/app/modules/admin/Services/admin.service.ts
+++ src/app/modules/admin/Services/admin.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASIC_URL ="https://quizbackend-production-fc51.up.railway.app/"
+const BASIC_URL ="http://65.0.207.242:8080/"
 
 @Injectable({
   providedIn: 'root'
--- src/app/modules/user/Services/test.service.ts
+++ src/app/modules/user/Services/test.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserStorageService } from '../../auth/services/user-storage.service';
 
-const BASIC_URL ="https://quizbackend-production-fc51.up.railway.app/";
+const BASIC_URL ="http://65.0.207.242:8080/";
 
 @Injectable({
   providedIn: 'root'
